ae6c6f235288c5aa08bedf1450cdcb3030e4a6a4
Julian Bolaños <julian.bolanos.guerrero@gmail.com>
2022-08-14 00:10:34 -0500

    Modificacion funcion 'index' para obtener datos de DataBase

--- mail.py
+++ mail.py
@@ -1,9 +1,16 @@
 from flask import (
     Blueprint, render_template
 )
+from miler.db import get_db 
+import mysql.connector
 
 bp = Blueprint('mail', __name__, url_prefix = '/')
 
 @bp.route('/', methods = ['GET'])
 def index():
-    return render_template('mails/index.html')
+    db, c = get_db()
+    c.execute('SELECT * FROM email')
+    mails = c.fetchall()
+    db.commit()
+
+    return render_template('mails/index.html', mails = mails)
